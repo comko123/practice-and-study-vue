372cdeb643da3778fa152fa7bf784cd2a78a49e0
Minseok Kim <comko123@naver.com>
2023-12-02 00:53:09 +0900

    fix : 버그 수정

--- src/components/main/index.vue
+++ src/components/main/index.vue
@@ -30,40 +30,28 @@ export default {
     <h1 class="font-bold text-2xl border-b border-indigo-400 py-2">To-Do Application</h1>
   </header>
   <main class="px-12">
-    <form
-      @submit="
-        $event.preventDefault()
-        addT()
-      "
-    >
+    <form @submit="$event.preventDefault();addT()">
       <input
         type="text"
         class="border-[3px] border-indigo-400 w-[80%] rounded-xl outline-none font-bold p-2"
-        @focusout="handleControl"
-      />
+        @focusout="handleControl"/>
       <input
         type="submit"
         value="SUBMIT"
         class="text-white font-bold bg-indigo-500 p-1 px-2 rounded-lg mx-2 hover:opacity-70"
       />
     </form>
-    <section
-      v-if="!data.data.length"
-      class="border-4 border-indigo-400 mt-4 h-[65vh] rounded-xl w-full flex justify-center items-center font-bold text-2xl text-indigo-400"
-    >
+    <section v-if="!data.data.length"
+      class="border-4 border-indigo-400 mt-4 h-[65vh] rounded-xl w-full flex justify-center items-center font-bold text-2xl text-indigo-400">
       등록한 일정이 없습니다
     </section>
     <section v-if="data.data.length">
-      <div
-        v-for="(item, index) in data.data"
-        :key="item.id"
-        class="flex my-4 w-[80%] justify-between border border-indigo-400 rounded-lg p-2"
-      >
+      <div v-for="(item, index) in data.data" :key="item.id"
+        class="flex my-4 w-[80%] justify-between border border-indigo-400 rounded-lg p-2">
         <div class="font-bold">{{ index + 1 }}. {{ item.body }} / {{ item.date }}</div>
         <button
           class="bg-rose-500 p-2 pt-[0.2rem] font-bold text-white rounded-full hover:opacity-70"
-          @click="deleteT(index)"
-        >
+          @click="deleteT(index)">
           x
         </button>
       </div>
